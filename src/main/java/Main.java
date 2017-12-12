c750dca28710a73e33b794d1f302861974904675
Anthony Dreessen <anthonydreessen@gmail.com>
2017-12-12 15:19:18 -0800

    compile jar with dependencies
    - add in invalid use tooltip

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,14 @@ import Models.BlockedIP;
 import Models.Duration;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
 
 import java.io.File;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
+@Parameters(separators = "=")
 public class Main {
 
     @Parameter(names = {"--file", "-f"}, converter = FileConverter.class)
@@ -41,13 +43,21 @@ public class Main {
 
                 databaseService.persistRecords(recordParser.parse());
 
-            } else { // Find limit violators mode
+            } else if (startDate != null && duration != null && threshold > 0){ // Find limit violators mode
                 List<BlockedIP> blockedIPs = databaseService.findViolatingIPAddresses(startDate, duration, threshold);
                 databaseService.persistBlockedIPs(blockedIPs);
                 for (BlockedIP ip : blockedIPs) {
                     System.out.print(ip);
                     System.out.println();
                 }
+            } else {
+                System.out.print("\nInvalid usage. You must specify either a file or a " +
+                        "start-time, duration, threshold combination\n\n" +
+                        "Valid Usage:\n " +
+                        "\t--file=, -f=\t\tspecify a path to the log file to build the database\n" +
+                        "\t--startDate\t\t\tspecify a start time for audit in form yyyy-MM-dd.HH:mm:ss\n" +
+                        "\t--duration\t\t\tspecify a duration for audit. Either 'daily' or 'hourly defaults to daily\n" +
+                        "\t--threshold\t\t\tspecify a threshold for audit.");
             }
         } catch (Exception e) {
             e.printStackTrace();
